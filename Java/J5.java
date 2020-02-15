d36cd55baaba307ff0154cbc09ef21471907a0f1
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-02-15 10:34:26 +0530

    initial commit

---
+++ Java/J5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class J5 {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                System.out.println(s1+"\t\t\t"+x);
+            }
+            System.out.println("================================");       
+
+    }
+}
+
+
+
