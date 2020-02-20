4fac8d004f83fa0fc453942d1252ade73ec6f236
kushal Singh Rathore <54983775+kushalsingh-00@users.noreply.github.com>
2020-02-20 21:29:55 +0530

    problem 18

---
+++ Java/J18.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class J18 {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        if(a.length()!=b.length())
+            return false;
+        char c[]=a.toCharArray();
+        char d[]=b.toCharArray();
+        Arrays.sort(c);
+        Arrays.sort(d);
+        for(int i=0;i<a.length();i++)
+            if(c[i]!=d[i])
+                return false;
+        
+        return true;
+        
+    }
+
+  public static void main(final String[] args) {
+    
+        final Scanner scan = new Scanner(System.in);
+        final String a = scan.next();
+        final String b = scan.next();
+        scan.close();
+        final boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
